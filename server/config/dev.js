c47a3a0dfae11cf1efca75ae1455b810617abf65
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-05-11 23:44:09 +0300

    Babel was set to use ES6 Modules from 'config' folder conditionally

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+const settings = {
+  mongo_url: "mongodb+srv://Alyona:12345@cluster0.c2xsj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  port: process.env.PORT || 5000
+};
+
+export default settings;
