f918e292c8ab1860a5912957d71a50ea9985d8f5
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-05-11 23:52:19 +0300

    Mongoose successfully connected with MongoDB and server runs on the port

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose, { connection } from 'mongoose';
+import cors from 'cors';
+import settings from './config';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+//Mongoose connection to MongoDB
+mongoose.connect(settings.mongo_url, {
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+}).then(()=>{
+    app.listen(settings.port, ()=>{
+        console.log(`Server running on port: ${ settings.port }`)
+    });
+}).catch(()=>{
+    console.log("Error ",error.message);
+});
+mongoose.set('useFindAndModify', false);
+
