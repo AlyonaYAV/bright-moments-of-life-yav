addfac7a421ad83b238f45e341f3a2c07f90b32f
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-05-12 18:37:35 +0300

    Post Route is registered and Route and Controller to get all posts are created

---
+++ server/controllers/posts.js
@@ -0,0 +1,11 @@
+import PostMessage from './../models/postMessage';
+export const getPosts = async (req, res) => {
+    try{
+      const postMessages = await PostMessage.find();
+      //console.log('All messages', postMessages);
+
+      res.status(200).json(postMessages);
+    }catch(e){
+      res.status(200).json({ message: e.message });
+    }
+  }
