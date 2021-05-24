b980840a25af3ea8a4aeab019d7f9bd4070adf47
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-05-25 02:56:26 +0300

    'Posts' obtains and state of 'App' component through the props and passes to the child component

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,28 +4,27 @@ import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './Post/Post';
 import useStyles from './styles';
 
-const Posts = ()=>{
-    const classes = useStyles();
-    //The state is the whole global Redux store
-    const posts = useSelector((state)=>{
-        //'posts' - from 'combineReducers({ posts: posts })' where it's a key 
-        return state.posts;
-    });
-    console.log("Posts ",posts);
-    return (
-      !posts.length ? <CircularProgress /> : (
-          <Grid className={ classes.container } 
-            container
-            alignItems="stretch"
-            spacing={3}
-          >
-          { posts.map( post => (
-            <Grid key={post._id} item xs={12} sm={6}>
-              <Post post={post} />
-            </Grid>))}
-        </Grid>
-      )
-    );
+const Posts = ({ setCurrentId })=>{
+  const classes = useStyles();
+  //The state is the whole global Redux store
+  const posts = useSelector((state)=>{
+    //'posts' - from 'combineReducers({ posts: posts })' where it's a key
+    return state.posts;
+  });
+  return (
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={ classes.container } 
+        container
+        alignItems="stretch"
+        spacing={3}
+      >
+      { posts.map( post => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} setCurrentId={ setCurrentId } />
+        </Grid>))}
+      </Grid>
+    )
+  );
 };
 
 export default Posts;
