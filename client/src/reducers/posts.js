ef1e3acc253d0af99d351ce14b84aa14ea41b055
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-05-25 22:04:01 +0300

    The Reducer that uses 'action creator' to make 'like' a post was ready

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,7 +4,8 @@ const posts = (state = initialState, action)=>{
   switch(action.type){
     case "FETCH_ALL" : return action.payload;
     case "CREATE_POST" : return [ ...state, action.payload ];
-    case "UPDATE_POST" : return state.map( post => post._id === action.payload._id ? action.payload : post);
+    case "UPDATE_POST" : return state.map( post => post._id === action.payload._id ? action.payload : post); 
+    case "LIKE_POST" : return state.map( post => post._id === action.payload.postUpdated._id ? action.payload.postUpdated : post);
     case "DELETE_POST" : return state.filter( post => post._id !== action.payload ? true : false);
     default: return state;
   }
