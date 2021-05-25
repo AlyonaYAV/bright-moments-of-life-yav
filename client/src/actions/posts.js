ef18ce1d87af4a07f7b312c8ee9dfe5b605b514b
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-05-25 23:05:55 +0300

    The 'action creators' and reducers for Posts have own action types in another file

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,11 @@
 import * as api from './../api';
+import {
+    FETCH_ALL_POST,
+    CREATE_POST,
+    UPDATE_POST,
+    DELETE_POST,
+    LIKE_POST
+} from './../constants/actionTypes';
 /*
 //Action creator without 'redux-thunk'
 const getPosts = () => {
@@ -19,7 +26,7 @@ the content of the 'action creator' with a function, this function contains 'dis
 export const getPosts = () => async (dispatch) => {
     try{
         const { data } = await api.fetchPosts();
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL_POST, payload: data });
     }catch(e){
         console.log(e.message);
     }
@@ -27,7 +34,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
     try{
         const { data } = await api.createPost(post);
-        dispatch({ type: 'CREATE_POST', payload: data });
+        dispatch({ type: CREATE_POST, payload: data });
     }catch(e){
         console.log(e.message);
     }
@@ -35,7 +42,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try{
         const { data } = await api.updatePost(id, post);
-        dispatch({ type: 'UPDATE_POST', payload: data });
+        dispatch({ type: UPDATE_POST, payload: data });
     }catch(e){
         console.log(e.message);
     }
@@ -43,7 +50,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
     try{
         await api.deletePost(id);
-        dispatch({ type: 'DELETE_POST', payload: id });
+        dispatch({ type: DELETE_POST, payload: id });
     }catch(e){
         console.log(e.message);
     }
@@ -51,7 +58,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
     try{
         const {data} = await api.likePost(id);
-        dispatch({ type: 'LIKE_POST', payload: data });
+        dispatch({ type: LIKE_POST, payload: data });
     }catch(e){
         console.log(e.message);
     }
