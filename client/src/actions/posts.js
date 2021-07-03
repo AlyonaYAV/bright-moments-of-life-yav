563072d1685d7c77a385ffb6012820c707fd4e06
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-07-03 23:24:38 +0300

    Object history is available in action creators for redirects

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -22,44 +22,55 @@ the posts that's why we should use 'redux-thunk'
     'redux-thunk'allows us to use the 'async / await' capabilities, it wraps
 the content of the 'action creator' with a function, this function contains 'dispatch'
 */
+//
+const unauthorized = (e, dispatch, history)=>{
+    if(e?.response?.status === 401){
+        dispatch({ type: 'LOGOUT' });
+        history.push('/');
+    }
+};
 // Action Creators
 export const getPosts = () => async (dispatch) => {
     try{
         const { data } = await api.fetchPosts();
         dispatch({ type: FETCH_ALL_POST, payload: data });
     }catch(e){
-        console.log(e.message);
+        //console.log(e.message);
     }
 }
-export const createPost = (post) => async (dispatch) => {
+export const createPost = (post, history) => async (dispatch) => {
     try{
         const { data } = await api.createPost(post);
         dispatch({ type: CREATE_POST, payload: data });
     }catch(e){
-        console.log(e.message);
+        //console.log(e.message);
+        unauthorized(e, dispatch, history);
     }
 }
-export const updatePost = (id, post) => async (dispatch) => {
+export const updatePost = (id, post, history) => async (dispatch) => {
     try{
         const { data } = await api.updatePost(id, post);
         dispatch({ type: UPDATE_POST, payload: data });
     }catch(e){
-        console.log(e.message);
+        //console.log(e.message);
+        unauthorized(e, dispatch, history);
     }
 }
-export const deletePost = (id) => async (dispatch) => {
+export const deletePost = (id, history) => async (dispatch) => {
     try{
         await api.deletePost(id);
         dispatch({ type: DELETE_POST, payload: id });
     }catch(e){
-        console.log(e.message);
+        //console.log(e.message);
+        unauthorized(e, dispatch, history);
     }
 }
-export const likePost = (id) => async (dispatch) => {
+export const likePost = (id, history) => async (dispatch) => {
     try{
         const {data} = await api.likePost(id);
         dispatch({ type: LIKE_POST, payload: data });
     }catch(e){
-        console.log(e.message);
+        //console.log("Error ",e.message);
+        unauthorized(e, dispatch, history);
     }
 }
