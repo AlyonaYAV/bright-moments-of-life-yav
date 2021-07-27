b35ad3364fb08329926f364b3a426f3a0db476bf
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-07-28 01:26:51 +0300

    Controller for searching by 'title' and 'tags' has been created

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,77 +1,96 @@
-import mongoose  from 'mongoose';
+import mongoose from 'mongoose';
 import PostMessage from './../models/postMessage';
 
-export const getPosts = async (req, res) => {
+export const getPosts = async (req, res)=>{
     try{
-      const postMessages = await PostMessage.find();
-      //console.log('All messages', postMessages);
-
-      res.status(200).json(postMessages);
+        const postMessages = await PostMessage.find();
+        //console.log('All messages ', postMessages);
+        
+        res.status(200).json(postMessages);
     }catch(e){
-      res.status(404).json({ message: e.message });
+        res.status(404).json({ message: e.message });
     }
-  }
+}
 
-  export const createPost = async (req, res)=>{
+export const createPost = async (req, res)=>{
     const post = req.body;
     const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
     try{
-      await newPost.save();
-      res.status(201).json(newPost);
+        await newPost.save();
+        res.status(201).json(newPost);
     }catch(e){
-      //The HTTP 409 Conflict response status code indicates a request conflict with current state of the target resource.
-      res.status(409).json({ message: e.message });
+        //The HTTP 409 Conflict response status code indicates a request conflict with current state of the target resource
+        res.status(409).json({ message: e.message });
     }
-  }
+}
 
-  export const updatePost = async (req, res)=>{
+export const updatePost = async (req, res)=>{
     const { id } = req.params;
     const post = req.body;
-    // Check is 'id' is mongoose id
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("There is no post");
+    //Checking 'id' is mongoose id
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('There is no post');
     try{
-      const {tags, creator, title, message, selectedFile} = post;
-      const updatedPost = await PostMessage.findByIdAndUpdate(id, {
-        tags,creator, title, message, selectedFile, id
-      }, { new: true });
-      res.status(200).json(updatedPost);
+        const {tags, creator, title, message, selectedFile} = post;
+        const updatedPost = await PostMessage.findByIdAndUpdate(id, {
+            tags, creator, title, message, selectedFile, id
+        }, { new : true });
+        res.status(200).json(updatedPost);
     }catch(e){
-      res.status(404).json({ message: e.message });
+        res.status(404).json({ message: e.message });
     }
-  }
+};
 
-  export const deletePost = async (req, res)=>{
+export const deletePost = async (req, res)=>{
     const { id: _id } = req.params;
-    // Check is '_id' is mongoose id
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("There is no post");
+    //Checking '_id' is mongoose id
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('There is no post');
     try{
-      await PostMessage.findByIdAndRemove(_id);
-      res.status(200).json({ message: "Post deleted successfully" });
+        await PostMessage.findByIdAndRemove(_id);
+        res.status(200).json({ message: "Post deleted successfully" });
     }catch(e){
-      res.status(404).json({ message: e.message });
+        res.status(404).json({ message: e.message });
     }
-  }
+};
 
-  export const likePost = async (req, res)=>{
+export const likePost = async (req, res)=>{
     const { id: _id } = req.params;
-    if(!req.userId) return res.status(401).json({ message: "Unauthenticated" });
-    // Check is '_id' is mongoose id
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("There is no post");
+    if(!req.userId) return res.status(401).json({ message: "Unathorized" });
+    //Checking '_id' is mongoose id
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('There is no post');
     try{
-      const post = await PostMessage.findById(_id);
-      // Check if the User is already liked a post
-      const index = post.likes.findIndex(id => id === String(req.userId));
-      // If a person has not liked the post yet
-      if(index === -1){
-        post.likes.push(req.userId)
-      }else{
-        // Remove person's like
-        post.likes = post.likes.filter(id => id !== String(req.userId));
-      }
-      // The 'post' beneath is instead of this - { likeCount: (post.likeCount + 1) }
-      const postUpdated = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
-      res.status(200).json({ message: "Post updated successfully", postUpdated });
+        const post = await PostMessage.findById(_id);
+        //Check if user is already liked a post
+        const index = post.likes.findIndex(id => id === String(req.userId));
+        //If a person has not liked the post yet
+        if(index === -1){
+            post.likes.push(req.userId);
+        }else{
+            //Remove person's like
+            post.likes = post.likes.filter(id => id !== String(req.userId));
+        }
+        //The 'post' beneath is instead of this - { likeCount: (post.likeCount + 1) }
+        const postUpdated = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+        res.status(200).json({ message: "Post updated successfully", postUpdated });
     }catch(e){
-      res.status(404).json({ message: e.message });
+        res.status(404).json({ message: e.message });
     }
-  }
+};
+
+// QUERY   /posts?page=1  it means page = 1  req.query.page = 1
+// PARAMS  /posts/:id  If  /posts/1  it means  req.params.id = 1      
+export const getPostBySearch = async (req, res)=>{
+    const { searchQuery, tags } = req.query;
+    try{ 
+        const title = new RegExp(searchQuery, 'i');
+        //Find all the Posts that matched one of those two criteria
+        // $or - means one from the array
+        // $in - operator takes an array as its value. {"breed" : { $in : ["Pitbull", "Great Dane", "Pug"]}}
+        const posts = await PostMessage.find({ $or: [
+            { title },
+            { tags: { $in: tags.split(',') } }
+        ] });
+        res.status(200).json({ data: posts });
+    }catch(e){
+        res.status(404).json({ message: e.message });
+    }
+};
