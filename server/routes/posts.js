94a137583ceeb1b3652e22ab6645c4ddf2d5e134
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-07-28 01:25:46 +0300

    Route for searching by 'title' and 'tags' has been created

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {
-  getPosts,
-  createPost,
-  updatePost,
-  deletePost,
-  likePost
+import { 
+    getPosts,
+    createPost,
+    updatePost,
+    deletePost,
+    likePost,
+    getPostBySearch
 } from './../controllers/posts';
 
 import auth from './../middleware/auth';
@@ -15,11 +16,13 @@ const router = express.Router();
 router.get('/', getPosts);
 // http://localhost:5000/posts
 router.post('/', auth, createPost);
-// http://localhost:5000/posts/7
+// http://localhost:5000/posts/12
 router.patch('/:id', auth, updatePost);
-// http://localhost:5000/posts/7
+// http://localhost:5000/posts/12
 router.delete('/:id', auth, deletePost);
-// http://localhost:5000/posts/7/likePost
+// http://localhost:5000/posts/12/likePost
 router.patch('/:id/likePost', auth, likePost);
+// // http://localhost:5000/posts/search?searchQuery=val1&tags=val2
+router.get('/search', getPostBySearch);
 
 export default router;
