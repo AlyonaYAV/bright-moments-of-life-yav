c5a74ec0e3e8bf0a143298714c6e1baccf1b697f
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-07-29 19:38:04 +0300

    Action creator to get API request and dispatch action

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,26 +1,28 @@
 import * as api from './../api';
-import {
+import { 
     FETCH_ALL_POST,
     CREATE_POST,
     UPDATE_POST,
     DELETE_POST,
-    LIKE_POST
-} from './../constants/actionTypes';
+    LIKE_POST,
+    FETCH_BY_SEARCH
+ } from './../constants/actionTypes';
 /*
 //Action creator without 'redux-thunk'
-const getPosts = () => {
+const getPosts = ()=> {
     const action = { type: 'FETCH_ALL', payload: [] };
     return action;
 }
 //With 'redux-thunk'
-const getPosts = () => async (dispatch)=>{
+const getPosts = ()=> async (dispatch)=> {
     const action = { type: 'FETCH_ALL', payload: [] };
     return action;
 }
-The problem is we are working with asynchronous data to actually fetch all
+  The problem is we are working with asynchronous data to actually fetch all
 the posts that's why we should use 'redux-thunk'
-    'redux-thunk'allows us to use the 'async / await' capabilities, it wraps
-the content of the 'action creator' with a function, this function contains 'dispatch'
+  'redux-thunk' allows us to use the 'async / await' capabilities, it wraps 
+the content of the 'action cretor' with a function, this function contains 
+'dispatch'
 */
 //
 const unauthorized = (e, dispatch, history)=>{
@@ -29,8 +31,8 @@ const unauthorized = (e, dispatch, history)=>{
         history.push('/');
     }
 };
-// Action Creators
-export const getPosts = () => async (dispatch) => {
+//Action Creators
+export const getPosts = ()=> async (dispatch)=> {
     try{
         const { data } = await api.fetchPosts();
         dispatch({ type: FETCH_ALL_POST, payload: data });
@@ -38,7 +40,16 @@ export const getPosts = () => async (dispatch) => {
         //console.log(e.message);
     }
 }
-export const createPost = (post, history) => async (dispatch) => {
+export const getPostBySearch = (searchQuery)=> async (dispatch)=> {
+    try{
+        const { data: { data } } = await api.fetchPostBySearch(searchQuery);
+        //console.log("Data ",data);
+        dispatch({ type: FETCH_BY_SEARCH, payload: data });
+    }catch(e){
+        //console.log(e.message);
+    }
+}
+export const createPost = (post, history)=> async (dispatch)=> {
     try{
         const { data } = await api.createPost(post);
         dispatch({ type: CREATE_POST, payload: data });
@@ -47,7 +58,7 @@ export const createPost = (post, history) => async (dispatch) => {
         unauthorized(e, dispatch, history);
     }
 }
-export const updatePost = (id, post, history) => async (dispatch) => {
+export const updatePost = (id, post, history)=> async (dispatch)=> {
     try{
         const { data } = await api.updatePost(id, post);
         dispatch({ type: UPDATE_POST, payload: data });
@@ -56,7 +67,7 @@ export const updatePost = (id, post, history) => async (dispatch) => {
         unauthorized(e, dispatch, history);
     }
 }
-export const deletePost = (id, history) => async (dispatch) => {
+export const deletePost = (id, history)=> async (dispatch)=> {
     try{
         await api.deletePost(id);
         dispatch({ type: DELETE_POST, payload: id });
@@ -65,9 +76,10 @@ export const deletePost = (id, history) => async (dispatch) => {
         unauthorized(e, dispatch, history);
     }
 }
-export const likePost = (id, history) => async (dispatch) => {
+export const likePost = (id, history)=> async (dispatch)=> {
     try{
         const {data} = await api.likePost(id);
+        //console.log("Data ",data);
         dispatch({ type: LIKE_POST, payload: data });
     }catch(e){
         //console.log("Error ",e.message);
