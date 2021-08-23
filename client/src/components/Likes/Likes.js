a7009e3c5193249ac6a369acb193073ba3a546ba
AlyonaYAV <alyonayavorskaya03@gmail.com>
2021-08-23 21:34:52 +0300

    Component 'Likes' just stores one like or dislike for a post from one user when he clicks on the button

--- client/src/components/Likes/Likes.js
+++ client/src/components/Likes/Likes.js
@@ -1,18 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import { Button } from '@material-ui/core';
 import { ThumbUpAltOutlined } from '@material-ui/icons';
+import usePrev from './../../hooks/usePrevious';
 
-const Likes = ({ user, post })=>{
-    const likesLength = post.likes.length;
-    if(likesLength > 0){
-      return post.likes.find( like => like === (user?.result?.googleId || user?.result?._id ) )
-      ? (<>
-          <ThumbUpAltIcon  fontSize="small" />&nbsp;{ likesLength > 2 ? `You and ${likesLength - 1} others` : `${likesLength} likes${likesLength > 1 ? 's' : ''}`}
-        </>)
-      : (<>
-          <ThumbUpAltIcon  fontSize="small" />&nbsp;{ likesLength } { likesLength === 1 ? 'Like' : 'Likes'}
-      </>)
+const Likes = ({ userId, post, likeWatcher })=>{
+
+  //It is good technique to make 'like' process smooth and more intense  
+  const [likes, setLikes] = useState(post?.likes);
+  //false - zero likes, true - one like
+  const [isLiked, setIsLiked] = useState(false);
+  //const prevLike = useRef();
+
+  const handleLikeClick = (e)=>{
+    //false - zero likes, true - one like
+    let isLikedPost = !!likes.find( like => like === userId );
+    //If 'like' is absent
+    if(!isLikedPost){
+      setLikes([ ...likes, userId ]);
+    }else{
+      setLikes(likes.filter( likeId => likeId !== userId ));
     }
-    return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>
-  };
-  export default Likes;
+    //Update previous data
+    setIsLiked(!isLikedPost);
+    //Call parent function
+    likeWatcher(!isLikedPost);
+  }
+  //componentDidMount - It is called only once
+  useEffect(()=>{
+    //false - zero likes, true - one like
+    let isLikedPost = !!likes.find( like => like === userId );
+    //Set initial state
+    setIsLiked(isLikedPost);
+    //Call parent function
+    likeWatcher(isLikedPost, true);
+  },[]);
+  //componentDidmount, componentDidUpdate
+  /*useEffect(()=>{
+    prevLike.current = isLiked;
+  }, [ isLiked ]);*/
+  //Use custom hook
+  usePrev(isLiked);
+
+  const likesLength = likes.length;
+    if(likesLength > 0){
+      let likeJSX = null; 
+      likeJSX = likes.find( like => like === userId )
+      ? (<>
+        <ThumbUpAltIcon fontSize="small" />&nbsp;{ likesLength > 2 ? `You and ${likesLength - 1} others` : `${likesLength} like${likesLength > 1 ? 's' : ''}` }
+        </>)
+      : (
+        <>
+        <ThumbUpAltOutlined fontSize="small" />&nbsp;{likesLength} { likesLength === 1 ? 'Like' : 'Likes'}
+        </>)
+      return <Button size="small" color="primary" 
+                disabled={ !userId }
+                onClick={ handleLikeClick }>
+                { likeJSX }
+              </Button>;
+    }
+    return (<Button size="small" color="primary" 
+              disabled={ !userId }
+              onClick={ handleLikeClick }>
+              <ThumbUpAltOutlined fontSize="small" />&nbsp;Like
+            </Button>);
+};
+
+export default Likes;
